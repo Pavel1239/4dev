136a6fc1b9547909b4afa724e583360fa956c583
Pavel1239 <32023278+Pavel1239@users.noreply.github.com>
2018-09-11 12:31:46 +0300

    Update game.php

--- game.php
+++ game.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/main.css?1">
-
+    <script type="text/javascript" src="/js/script.js"></script>
     <title>4DEV</title>
 </head>
 <body>
